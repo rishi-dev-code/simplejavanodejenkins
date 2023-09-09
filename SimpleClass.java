b3afe2bbcaa60c9ddb6f0573bc8a131364c12f00
Rishi <email4rishi@fastmail.com>
2023-09-09 19:40:01 +1000

    scmtest

--- SimpleClass.java
+++ SimpleClass.java
@@ -7,5 +7,6 @@ public class SimpleClass {
         System.out.println("Second Line");
         System.out.println(new Date());
         System.out.println("Added");
+        System.out.println("Added for SCM test ");
     }
 }
