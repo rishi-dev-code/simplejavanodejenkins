54c9d4512eea005e3452c2c61d28b5f2396871e2
Rishi <email4rishi@fastmail.com>
2023-09-09 17:58:20 +1000

    first commit

---
+++ SimpleClass.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class SimpleClass {
+
+    public static void main(String[] args) {
+        System.out.println("First Line");
+        System.out.println("Second Line");
+        System.out.println(new Date());
+    }
+}
