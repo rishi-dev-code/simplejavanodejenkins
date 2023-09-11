11a91fdf4953d997e913a021c8783c20b7cb0d47
Rishi <email4rishi@fastmail.com>
2023-09-12 03:20:45 +1000

    aws

--- testnode.js
+++ testnode.js
@@ -1,5 +1,5 @@
 console.log("This is just a simple node console");
 console.log("Lets run on jenkins");
 console.log("my node path");
-console.log("Added for SCM test");
+console.log("Added for SCM test ");
 console.log("Added for SCM test AWS");
